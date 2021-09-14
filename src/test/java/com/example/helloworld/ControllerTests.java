d742a5aa804ae1c63b1e5b338d6e8e5b15206a28
Polina Mezenina <p.mezenina@devinotele.com>
2021-09-14 16:34:46 +0300

    add tests

--- src/test/java/com/example/helloworld/ControllerTests.java
+++ src/test/java/com/example/helloworld/ControllerTests.java
@@ -1,19 +1,30 @@
 package com.example.helloworld;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ControllerTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private Controller controller;
+
     private String HELLO_WORLD = "Hello world";
 
     @Test
-    void printHelloWorldTest() {
-        Controller controller = new Controller();
+     public void printHelloWorldTest() {
         assertEquals(
                 controller.printHelloWorld().getStatusCode(),
                 new ResponseEntity<>(HELLO_WORLD, HttpStatus.OK).getStatusCode());
@@ -22,4 +33,11 @@ class ControllerTests {
                 HELLO_WORLD
         );
     }
+
+    @Test
+    public void helloWorldShouldReturnDefaultMessage() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello world");
+    }
+
 }
